a039ac1b6e1eaca15b12070a048273042da9a92b
JivkoJelev91 <zhelev.zhivko.1991@gmail.com>
2019-04-08 17:45:36 +0300

    react-trello-test

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 5432;
+
+const db = require('./config/db');
+// const Gig = require('./models/Gig');
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+app.use(cors());
+app.options('*', cors()); // include before other routes
+app.use(morgan('dev'));
+
+
+// routes
+require('./routes/routes')(app);
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
---
+++ server/config/db.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const Lanes = require('../models/Lanes');
+const Cards = require('../models/Cards');
+
+const uri = 'mongodb://localhost:27017/react-sprello' 
+
+mongoose.connect(uri).then(
+    () => { 
+        console.log('Connected to Mongo');
+        Lanes.getInit().then(() => {
+            console.log('Database ready');                
+        }).catch((reason) => {
+            console.log('Something went wrong');
+            console.log(reason);
+        });
+    },
+    err => {
+         /** handle initial connection error */ 
+         console.log('error connecting to Mongo: ')
+         console.log(err);
+        }
+  );
+
+module.exports = mongoose.connection;
